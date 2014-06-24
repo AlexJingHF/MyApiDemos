dde6c79a901f9888cba6172ea49a521b248368d2
JingHaifeng <huq33@126.com>
2014-06-24 10:12:59 +0800

    add AnimationActivityDemo

--- src/com/example/alex/myapidemos/app/AnimationActivity.java
+++ src/com/example/alex/myapidemos/app/AnimationActivity.java
@@ -1,6 +1,8 @@
 package com.example.alex.myapidemos.app;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,10 +17,10 @@ public class AnimationActivity extends BaseActivity implements View.OnClickListe
 		setContentView(R.layout.activity_animation);
 		findViewById(R.id.fade_animation).setOnClickListener(this);
 		findViewById(R.id.zoom_animation).setOnClickListener(this);
-		findViewById(R.id.modern_fade_animation);
-		findViewById(R.id.modern_zoom_animation);
-		findViewById(R.id.scale_up_animation);
-		findViewById(R.id.zoom_thumbnail_animation);
+		findViewById(R.id.modern_fade_animation).setOnClickListener(this);
+		findViewById(R.id.modern_zoom_animation).setOnClickListener(this);
+		findViewById(R.id.scale_up_animation).setOnClickListener(this);
+		findViewById(R.id.zoom_thumbnail_animation).setOnClickListener(this);
 	}
 
 	@Override
@@ -34,16 +36,26 @@ public class AnimationActivity extends BaseActivity implements View.OnClickListe
 			overridePendingTransition(R.anim.zoom_enter, R.anim.zoom_exit);
 			break;
 		case R.id.modern_fade_animation:
-			
+			ActivityOptions options = ActivityOptions.makeCustomAnimation(this, R.anim.fade, R.anim.hold);
+			startActivity(intent, options.toBundle());
 			break;
 		case R.id.modern_zoom_animation:
-			
+			ActivityOptions options2 =ActivityOptions.makeCustomAnimation(this, R.anim.zoom_enter, R.anim.zoom_exit);
+			startActivity(intent,options2.toBundle());
 			break;
 		case R.id.scale_up_animation:
-			
+			ActivityOptions options3 = ActivityOptions.makeScaleUpAnimation(v, 0, 0, v.getWidth(), v.getHeight());
+			startActivity(intent, options3.toBundle());
 			break;
 		case R.id.zoom_thumbnail_animation:
-			
+			v.setDrawingCacheEnabled(true);
+			v.setPressed(false);
+			v.refreshDrawableState();
+			Bitmap bitmap = v.getDrawingCache();
+//			Canvas canvas = new Canvas(bitmap);
+			ActivityOptions options4 = ActivityOptions.makeThumbnailScaleUpAnimation(v, bitmap, 0, 0);
+			startActivity(intent, options4.toBundle());
+			v.getDrawingCache(false);
 			break;
 			
 		default:
