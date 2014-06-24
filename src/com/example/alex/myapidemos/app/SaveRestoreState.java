746db39c21e7e38ce208cd4bd0454d2a701103ab
JingHaifeng <huq33@126.com>
2014-06-24 18:07:31 +0800

    add save&RestoreState

---
+++ src/com/example/alex/myapidemos/app/SaveRestoreState.java
@@ -0,0 +1,15 @@
+package com.example.alex.myapidemos.app;
+
+import android.os.Bundle;
+
+import com.example.alex.myapidemos.R;
+import com.example.alex.myapidemos.base.BaseActivity;
+
+public class SaveRestoreState extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.save_restore_state);
+	}
+}
